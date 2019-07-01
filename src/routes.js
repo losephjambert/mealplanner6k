ccf181369183cf246c73c510bb769a2a640df4cc
Joe <josephc.lambert@gmail.com>
2019-07-01 13:21:30 -0700

    updates prettier line length limit

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,9 @@ const onRedirectCallback = appState => {
   window.history.replaceState(
     {},
     document.title,
-    appState && appState.targetUrl ? appState.targetUrl : window.location.pathname
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
   )
 }
 
